46540a08e9995343bb1762a96d95b0390cfa92a5
Tigran <tikonazaryan5@gmail.com>
2022-03-15 22:40:10 +0400

    Updates

---
+++ precache-manifest.d3a6a44173ab6926f101d0c117738e1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d000ff958494ab56fa19a41e6fd7997a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c4a185dc0b3ebdaec280",
+    "url": "./static/css/main.e5a6986b.chunk.css"
+  },
+  {
+    "revision": "633bb7a9ec29c97c319b",
+    "url": "./static/js/2.d6ffa4e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.d6ffa4e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4a185dc0b3ebdaec280",
+    "url": "./static/js/main.eb096fbf.chunk.js"
+  },
+  {
+    "revision": "120c79d413747780b492",
+    "url": "./static/js/runtime-main.14bcf8aa.js"
+  }
+]);
