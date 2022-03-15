94d784520ac382e23f000497eb2bf846a5b8a180
Tigran <tikonazaryan5@gmail.com>
2022-03-15 22:40:24 +0400

    Init

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <div class="header">
+        <h1>Simple Gallery Example</h1>
+        <p>
+          Follow <a>this</a> link to see same website with our service
+        </p>
+      </div>
+
+      <div class="row">
+        <div class="column">
+          <img src="https://www.w3schools.com/w3images/wedding.jpg" alt="" />
+          <img src="https://www.w3schools.com/w3images/rocks.jpg" alt="" />
+          <img src="https://www.w3schools.com/w3images/falls2.jpg" alt="" />
+          <img src="https://www.w3schools.com/w3images/paris.jpg" alt="" />
+          <img src="https://www.w3schools.com/w3images/nature.jpg" alt="" />
+          <img src="https://www.w3schools.com/w3images/mist.jpg" alt="" />
+          <img src="https://www.w3schools.com/w3images/paris.jpg" alt="" />
+        </div>
+        <div class="column">
+          <img src="https://www.w3schools.com/w3images/underwater.jpg" alt="" />
+          <img src="https://www.w3schools.com/w3images/ocean.jpg" alt="" />
+          <img src="https://www.w3schools.com/w3images/wedding.jpg" alt="" />
+          <img
+            src="https://www.w3schools.com/w3images/mountainskies.jpg"
+            alt=""
+          />
+          <img src="https://www.w3schools.com/w3images/rocks.jpg" alt="" />
+          <img src="https://www.w3schools.com/w3images/underwater.jpg" alt="" />
+        </div>
+        <div class="column">
+          <img src="https://www.w3schools.com/w3images/wedding.jpg" alt="" />
+          <img src="https://www.w3schools.com/w3images/rocks.jpg" alt="" />
+          <img src="https://www.w3schools.com/w3images/falls2.jpg" alt="" />
+          <img src="https://www.w3schools.com/w3images/paris.jpg" alt="" />
+          <img src="https://www.w3schools.com/w3images/nature.jpg" alt="" />
+          <img src="https://www.w3schools.com/w3images/mist.jpg" alt="" />
+          <img src="https://www.w3schools.com/w3images/paris.jpg" alt="" />
+        </div>
+        <div class="column">
+          <img src="https://www.w3schools.com/w3images/underwater.jpg" alt="" />
+          <img src="https://www.w3schools.com/w3images/ocean.jpg" alt="" />
+          <img src="https://www.w3schools.com/w3images/wedding.jpg" alt="" />
+          <img
+            src="https://www.w3schools.com/w3images/mountainskies.jpg"
+            alt=""
+          />
+          <img src="https://www.w3schools.com/w3images/rocks.jpg" alt="" />
+          <img src="https://www.w3schools.com/w3images/underwater.jpg" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
